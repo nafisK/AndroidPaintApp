1bcf8a73464319a202f47396ce34286e8e28a479
nafisK <nafisrizwank@gmail.com>
2022-01-15 18:57:51 -0500

    added brush size dialog box

--- app/src/main/java/com/example/androidpaintapp/MainActivity.kt
+++ app/src/main/java/com/example/androidpaintapp/MainActivity.kt
@@ -1,19 +1,58 @@
 package com.example.androidpaintapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
+    private var ib_brush: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawingView = findViewById(R.id.drawing_view)
+        ib_brush = findViewById(R.id.ib_brush)
+
+
         drawingView?.setSizeForBrush(20.toFloat())
 
+        ib_brush?.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
 
+
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        var brushDialog = Dialog(this)
+
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush Size: ")
+
+        val smallBtn = brushDialog.findViewById<ImageView>(R.id.ib_small_brush)
+        val mediumBtn = brushDialog.findViewById<ImageView>(R.id.ib_medium_brush)
+        val largeBtn = brushDialog.findViewById<ImageView>(R.id.ib_large_brush)
+
+        smallBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        mediumBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+
+        }
+        largeBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+
+        }
+
+        brushDialog.show()
     }
 }
